10153adbe1423a07f06ff3094d7cc6b0ab295b90
Andrew Bell <andrew@recursivechaos.com>
2016-02-18 18:57:02 -0600

    Basic link to login from home.

--- src/main/java/com/recursivechaos/Oauth2DemoApplication.java
+++ src/main/java/com/recursivechaos/Oauth2DemoApplication.java
@@ -3,12 +3,35 @@ package com.recursivechaos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @EnableOAuth2Sso
+@RestController
 @SpringBootApplication
-public class Oauth2DemoApplication {
+public class Oauth2DemoApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Oauth2DemoApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers("/", "/login**", "/webjars/**")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
     }
 }
